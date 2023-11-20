fe91f9d4038e04d9d95502587b7524d91e4e7f21
Nandeesh3000 <113001762+Nandeesh3000@users.noreply.github.com>
2023-11-20 14:41:07 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -19,8 +19,9 @@ class CalculatorTest(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(11, self.calc.add(3, 7), "The addition is wrong")
+   def test_add(self):
+    """Test the add method."""
+    self.assertEqual(10, self.calc.add(3, 7), "The addition is wrong")
 
     def test_subtract(self):
         self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
